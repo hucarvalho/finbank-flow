4ff639d2fcc130cf65b8fac6886d79a844fc538b
hugo <carvalhohugo425@gmail.com>
2025-08-01 00:16:47 -0300

    feat:(users) adicionar busca de historico

--- finbank-back/app/controllers/user_controller.py
+++ finbank-back/app/controllers/user_controller.py
@@ -1,10 +1,43 @@
 from flask import jsonify
 from app.models.user import User
 from flask_jwt_extended import create_access_token
+from app import db
 
 
+def get_user_history(user_id):
+    try:
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"error": "Usuário não encontrado"}), 404
+        
+        sql = db.text(f"""
+                    select transacoes.id, data_hora, valor, case when conta_remetente_id = {user_id} then 'transacao feita' when conta_destinatario_id = {user_id} then 'transação recebida' else 'nok' end as tipo from transacoes
+                    join contas
+                    on (contas.id = transacoes.conta_remetente_id or contas.id = transacoes.conta_destinatario_id) and contas.user_id = {user_id}
 
+                    union
 
+                    select e.id, data_contrato, valor_entregue, 'emprestimo' as tipo from emprestimos as e
+                    where user_id = {user_id}
+
+                    union
+
+                    select p.id, data_pagamento, p.valor, 'parcela' as tipo from parcelas as p
+                    join emprestimos e
+                    on e.id = p.emprestimo_id
+                    where e.user_id = {user_id} and status = 'paga' """)
+        result = db.session.execute(sql).fetchall()
+        history = []
+        for row in result:
+            history.append({
+                'id': row[0],
+                'data_hora': row[1].strftime('%Y-%m-%d %H:%M:%S'),
+                'valor': row[2],
+                'tipo': row[3]
+            })
+        return history
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 def get_user_with_contas_by_cpf(cpf):
     try:
